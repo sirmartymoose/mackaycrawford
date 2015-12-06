312f31182f2a2c8aa79454c7a3eacb2a0ade800b
sirmartymoose <sirmartymoose@gmail.com>
2015-12-06 17:00:04 +0000

    d

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 var server = app.listen(8080, function () {
   var host = server.address().address;
