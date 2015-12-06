36a857342573f53dd3efd57917015e42bd0e85f7
sirmartymoose <sirmartymoose@gmail.com>
2015-12-06 17:36:22 +0000

    serve static

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-
-var express = require('express');
-var app = express();
-
-app.use(express.static('/public'));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var express = require('express')
+var serveStatic = require('serve-static')
+ 
+var app = express()
+ 
+app.use(serveStatic('public'))
+app.listen(8080)
