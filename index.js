7e37c32707cb7a4c1736dc0485e7f8c306e0de98
sirmartymoose <sirmartymoose@gmail.com>
2015-12-06 17:14:10 +0000

    HI

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 var server = app.listen(8080, function () {
-  var host = '52.91.60.211';
+  var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
